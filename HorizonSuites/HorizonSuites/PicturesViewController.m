1bb04944189d55224d46fa8ad62cf60aa12bb863
jorgergb <user3747@74-80-237-104.ic2net.net>
2013-10-21 19:13:37 -0700

    galeria
    comienzo de la galeria
    se agrego el picturesViewController

---
+++ HorizonSuites/HorizonSuites/PicturesViewController.m
@@ -0,0 +1,38 @@
+//
+//  PicturesViewController.m
+//  HorizonSuites
+//
+//  Created by user3747 on 10/20/13.
+//  Copyright (c) 2013 Fernando Archila. All rights reserved.
+//
+
+#import "PicturesViewController.h"
+
+@interface PicturesViewController ()
+
+@end
+
+@implementation PicturesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
